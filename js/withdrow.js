be58c30cd604b8dd5c057ed37c7c31c9646c14bf
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-02-13 19:57:30 +0600

    added withdrow

--- js/withdrow.js
+++ js/withdrow.js
@@ -1,1 +1,16 @@
-console.log('withdrow file')
+document.getElementById('btn-withdrow').addEventListener('click', function(){
+
+    const newWithdrowAmount = getInputFieldValueById('withdrow-field');
+
+    const previousWithdrowTotal = getTextElementValueById('withdrow-total');
+
+    const newWithdrowTotal = previousWithdrowTotal + newWithdrowAmount;
+
+    setTextElementValueById('withdrow-total', newWithdrowTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+
+    const totalbalance = previousBalanceTotal - newWithdrowAmount;
+
+    setTextElementValueById('balance-total', totalbalance);
+})
